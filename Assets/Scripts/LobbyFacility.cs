ea3e5f12fa3f3d898091390fa7fed0724d98f398
Hyuni02 <jackjsh@naver.com>
2023-03-11 14:14:25 +0900

    상점 가구 추가/확대 구현

--- Assets/Scripts/LobbyFacility.cs
+++ Assets/Scripts/LobbyFacility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LobbyFacility : MonoBehaviour
 {
-    public enum FacilityType { Inventory, Sever, RestoreStation, Counter, Table}
+    public enum FacilityType { Inventory, Sever, RestoreStation, Counter, Table, Shop}
     public FacilityType facilityType;
 
     public Transform FocusPos;
