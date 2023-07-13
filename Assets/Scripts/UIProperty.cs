b0c03343a191267de19c0752b8bcfb2d88045906
Hyuni02 <jackjsh@naver.com>
2023-07-13 13:05:58 +0900

    아이템 드래그드롭 구현 중
    헬멧

--- Assets/Scripts/UIProperty.cs
+++ Assets/Scripts/UIProperty.cs
@@ -39,15 +39,52 @@ public class UIProperty : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     }
 
     public void OnDrop(PointerEventData eventData) {
-        print($"드래그한 오브젝트 : {eventData.pointerDrag.name}");
-        print($"드랍한 위치의 오브젝트 : {gameObject.name}");
+        GameObject drag = eventData.pointerDrag.gameObject;
+        GameObject drop = gameObject;
+        //print($"드래그한 오브젝트 : {drag.name}");
+        //print($"드랍한 위치의 오브젝트 : {drop.name}");
+
+        //foreach(var comp in drag.GetComponents<Component>()) {
+        //    print(comp.GetType());
+        //}
 
         //드랍할 수 없는 위치에 드랍 (상점 아이템 위)
         if (!b_dropable) {
-            print("드랍 불가능 위치");
+            if(drop.GetComponent<EquipmentUI>().equiped != null) {
+                print("해당 슬롯에는 이미 아이템이 존재함");
+            }
+            else {
+                print("드랍 불가능 위치");
+            }
             return;
         }
 
-        //드랍한 위치를 기반으로 드랍 가능한 타입 나열
+        //장착칸에 드랍
+        if (drop.CompareTag("UI_Slot")) {
+            switch(GetComponent<EquipmentUI>().itemtype) {
+                case Type.pri:
+                    break;
+                case Type.sec:
+                    break;
+                case Type.helmet:
+                    if (drag.GetComponent<Item_Helmet>() != null) {
+                        print($"{drag.name} -> {drop.name}");
+                        return;
+                    }
+                    break;
+                case Type.body: 
+                    break;
+                case Type.bag: 
+                    break;
+
+            }
+        }
+        //인벤에 드람
+        else {
+
+        }
+
+        print("유효하지 않은 이동");
+
     }
 }
